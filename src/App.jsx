a8adb03ebc15968271cbea0ab87e2ce1a3069caf
4fthab <afthabathz@gmail.com>
2023-03-04 13:54:05 +0530

    INITIAL COMMIT

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { ChakraProvider } from "@chakra-ui/react";
+function App() {
+  return (
+    <ChakraProvider>
+      <div>
+        <h1>hello world</h1>
+      </div>
+    </ChakraProvider>
+  );
+}
+
+export default App;
